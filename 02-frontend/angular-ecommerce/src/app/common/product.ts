c7c2094b171350f5a75a1f44bb459f9905d564ab
MaulikKP <patel.maulik334@gmail.com>
2022-02-02 16:02:26 -0500

    new file

---
+++ 02-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+    id: number;
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageURL: String;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    LastUpdated: Date;
+
+
+}
